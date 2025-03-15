293a8d441a3f3a7ad5f3fc78f49758c1ca31ae8f
Helia-Gholami <gheliagholami@gmail.com>
2025-03-15 14:42:56 +0330

    added js to search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert(`hello world!`);
+function searchHandle(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let city = document.querySelector("#city");
+  city.innerHTML = searchInput.value;
+}
+
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", searchHandle);
