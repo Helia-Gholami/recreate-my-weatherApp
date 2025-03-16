c707f94e4fd8e90c99d5ad33ec59d5600065253d
Helia-Gholami <gheliagholami@gmail.com>
2025-03-16 14:48:48 +0330

    added api and weather information

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(`#temperature`);
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+}
+
+function searchCity(city) {
+  let apiKey = `2a2d11ob41f23821caed90d45et0cd09`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchHandle(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
